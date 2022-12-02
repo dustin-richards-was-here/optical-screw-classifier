4c0b0c74789438bad6054b80845df1b0be473702
Dustin Richards <richards.dustinb@gmail.com>
2022-12-02 12:46:03 -0700

    add support for multiple images, adjust kernel to work on image without preprocessing

--- threadKernel.py
+++ threadKernel.py
@@ -2,35 +2,56 @@ from PIL import Image, ImageOps
 import numpy as np
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import sys
 
-lowerKernel = np.array(ImageOps.grayscale(Image.open("img/thread-kernel.png"))).astype(float) / 255
-upperKernel = np.array(ImageOps.grayscale(Image.open("img/thread-kernel.png")).rotate(180)).astype(float) / 255
-img = np.array(ImageOps.grayscale(Image.open("img/screw-thresh.png"))).astype(float) / 255
+def printUsage():
+    print("Usage: " + sys.argv[0] + " kernel img1 img2 img3 ...")
+
+if (len(sys.argv) < 3):
+    printUsage()
+
+
+kernelImg = sys.argv[1]
+lowerKernel = np.array(ImageOps.grayscale(Image.open(kernelImg))).astype(float) / 255
+upperKernel = np.array(ImageOps.grayscale(Image.open(kernelImg)).rotate(180)).astype(float) / 255
 
 lowerKernel = (lowerKernel - 0.5) * 2
 upperKernel = (upperKernel - 0.5) * 2
 
-lowerConv = ndimage.convolve(img, lowerKernel)
-upperConv = ndimage.convolve(img, upperKernel)
+imgs = []
 
-lowerConv = lowerConv / np.max(lowerConv)
-upperConv = upperConv / np.max(upperConv)
+for i in range(len(sys.argv) - 2):
+    file = sys.argv[i+2]
+    imgs.append(np.array(ImageOps.grayscale(Image.open(file))).astype(float) / 255)
 
-threshold = 0.9
-lowerThresh = lowerConv > threshold
-upperThresh = upperConv > threshold
-lowerConv[np.invert(lowerThresh)] = 0
-upperConv[np.invert(upperThresh)] = 0
+convs = []
 
-conv = lowerConv + upperConv
+for i in range(len(imgs)):
+    lowerConv = ndimage.convolve(imgs[i], lowerKernel)
+    upperConv = ndimage.convolve(imgs[i], upperKernel)
 
-subplotRows = 1
-subplotCols = 2
+    lowerConv = lowerConv / np.max(lowerConv)
+    upperConv = upperConv / np.max(upperConv)
 
-plt.subplot(subplotRows, subplotCols, 1)
-plt.imshow(conv)
+    threshold = 0.9
+    lowerThresh = lowerConv > threshold
+    upperThresh = upperConv > threshold
+    lowerConv[np.invert(lowerThresh)] = 0
+    upperConv[np.invert(upperThresh)] = 0
 
-plt.subplot(subplotRows, subplotCols, 2)
-plt.imshow(img)
+    convs.append(lowerConv + upperConv)
+
+subplotRows = 3
+subplotCols = len(imgs)
+
+for i in range(len(imgs)):
+    plt.subplot(subplotRows, subplotCols, i + 1)
+    plt.imshow(imgs[i])
+    
+    plt.subplot(subplotRows, subplotCols, i + 1 + len(imgs))
+    plt.imshow(convs[i])
+
+    plt.subplot(subplotRows, subplotCols, i + 1 + len(imgs) * 2)
+    plt.imshow(convs[i] + imgs[i])
 
 plt.show()
