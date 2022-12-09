a019fa40f9114d905a58aa07dd86d12db2c4e94c
Dustin Richards <richards.dustinb@gmail.com>
2022-12-09 01:37:56 -0700

    add sam's code to identify screw head and body regions

--- identify.py
+++ identify.py
@@ -94,9 +94,56 @@ def getIntensityToCenter(img: np.ndarray, slope: float, intercept: float, xLimit
 
     return intensities
 
-def printUsage():
-    print("Usage (images): " + sys.argv[0] + " kernel img1 img2 img3 ...")
-    print("Usage (camera): " + sys.argv[0] + " stream")
+def getScrewRegions(img, pad=0):
+    THRESH = 230 / 255
+    img_thresh = img < THRESH
+    mid_y = img.shape[0] // 2
+
+    # Find the end of the screw
+    # Find the first non-white pixel from right-to-left
+    dat_end = []
+    for img_row in img_thresh:
+        screw_coords = np.argwhere(img_row)
+        last_pix = 0
+        if len(screw_coords) > 0:
+            last_pix = screw_coords[-1,0]
+        dat_end.append(last_pix)
+    dat_end = np.array(dat_end)
+
+    screw_end = dat_end[mid_y]
+
+    # Find the start of the screw
+    screw_start = np.argwhere(img_thresh[mid_y])[0,0]
+
+    # The largest "jump" in end data is (probably) the edge
+    diffs = dat_end[1:] - dat_end[:-1]
+
+    # Get the top and bottom of the thread
+    edge_loc_top = np.argmax(diffs[:mid_y])
+    edge_loc_bottom = mid_y + np.argmin(diffs[mid_y:])+1
+
+    # Go a bit further until past any additional abrupt changes
+    orig_edge_loc = edge_loc_top
+    while diffs[edge_loc_top+1] > 0.1 * diffs[orig_edge_loc]:
+        edge_loc_top -= 1
+    orig_edge_loc = edge_loc_bottom
+    while diffs[edge_loc_bottom] < 0.1 * diffs[orig_edge_loc-1]:
+        edge_loc_bottom += 1
+
+    # Get the start of the thread
+    head_end = dat_end[edge_loc_top]
+
+    # Get the top/bottom of the head
+    head_top = np.argwhere(dat_end > (screw_start+2))[0,0]
+    head_bottom = np.argwhere(dat_end > (screw_start+2))[-1,0]+1
+
+    # Crop and return (head, screw)
+    #return [
+    #    img[head_top-pad:head_bottom+1+pad, screw_start-pad:head_end+1+pad], 
+    #    img[edge_loc_top-pad:edge_loc_bottom+1+pad, head_end-pad:screw_end+1+pad]
+    #    ]
+
+    return screw_start, head_end, screw_end
 
 def identify(screwImg: Image, kernelImg: Image, plotParams: dict = None):
     img = np.array(ImageOps.grayscale(screwImg)).astype(float) / 255
@@ -171,6 +218,9 @@ def identify(screwImg: Image, kernelImg: Image, plotParams: dict = None):
     threadPitchFrequency = abs(freqs[maxMagnitudeIndex])
     threadPitchPixels = 1 / threadPitchFrequency
 
+    headStartX, headEndX, threadsEndX = getScrewRegions(img)
+    print(headStartX, headEndX, threadsEndX)
+
     if (plotParams):
         subplotRows = 4
 
@@ -191,6 +241,9 @@ def identify(screwImg: Image, kernelImg: Image, plotParams: dict = None):
         plt.imshow(img)
         plt.plot(lowerLineY, color = 'yellow')
         plt.plot(upperLineY, color = 'magenta')
+        plt.plot((headStartX, headStartX), (0, img.shape[0]), color = 'red')
+        plt.plot((headEndX, headEndX), (0, img.shape[0]), color = 'red')
+        plt.plot((threadsEndX, threadsEndX), (0, img.shape[0]), color = 'red')
 
         plt.subplot(subplotRows, subplotCols, imageIndex + 1 + subplotCols * 2)
         plt.plot(lowerThreadFrequencySample)
@@ -202,6 +255,10 @@ def identify(screwImg: Image, kernelImg: Image, plotParams: dict = None):
             plt.show()
 
     return diameterPixels, threadPitchPixels
+
+def printUsage():
+    print("Usage (images): " + sys.argv[0] + " kernel img1 img2 img3 ...")
+    print("Usage (camera): " + sys.argv[0] + " stream")
 
 def main():
     if (len(sys.argv) < 3):
