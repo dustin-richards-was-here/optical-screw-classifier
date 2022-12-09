391c64d1f786ec98860fa5ca2055436bdf5505d4
Dustin Richards <richards.dustinb@gmail.com>
2022-12-08 21:58:08 -0700

    modularize code

--- threadKernel.py
+++ threadKernel.py
@@ -94,12 +94,8 @@ def getIntensityToCenter(img: np.ndarray, slope: float, intercept: float, xLimit
 def printUsage():
     print("Usage: " + sys.argv[0] + " kernel img1 img2 img3 ...")
 
-def main():
-    if (len(sys.argv) < 3):
-        printUsage()
-
-    kernelImgFile = sys.argv[1]
-    kernelImg = ImageOps.grayscale(Image.open(kernelImgFile))
+def identify(screwImg: Image, kernelImg: Image, plotParams: dict = None):
+    img = np.array(ImageOps.grayscale(screwImg)).astype(float) / 255
 
     lowerKernel = np.array(kernelImg).astype(float) / 255
     upperKernel = np.array(kernelImg.rotate(180)).astype(float) / 255
@@ -107,131 +103,123 @@ def main():
     lowerKernel = (lowerKernel - 0.5) * 2
     upperKernel = (upperKernel - 0.5) * 2
 
-    imgs = []
+    # convolve the image with the tread kernel and use RANSAC to find a cluster
+    #  of points that represents the threadform
+    # we do this twice because one of the kernels is upside down in order to
+    #  get the threads in the lower half of the image
+    lowerConv, lowerInliers, lowerSlope, lowerIntercept = convoleAndRANSAC(img, lowerKernel)
+    upperConv, upperInliers, upperSlope, upperIntercept = convoleAndRANSAC(img, upperKernel)
 
-    for i in range(len(sys.argv) - 2):
-        file = sys.argv[i+2]
-        imgs.append(np.array(ImageOps.grayscale(Image.open(file))).astype(float) / 255)
+    # combine the two convolution images, really just for visualizing
+    conv = lowerConv + upperConv
 
-    convs = []
-    lowerInliers = []
+    # calculate the points of a line for either side of the screw based on the
+    #  RANSAC results from above
+    x = np.arange(0, img.shape[1])
+    lowerLineY = lowerSlope * x + lowerIntercept
+    upperLineY = upperSlope * x + upperIntercept
 
-    upperInliers = []
-    lowerLineY = []
-    upperLineY = []
-    lowerIntensityToCenter = []
-    upperIntensityToCenter = []
-    lowerThreadFrequencySamples = []
-    ffts = []
-    diameters = []
+    # find the x coordinates of the furthest left and right inlier point
+    lowerXLimArgmin = np.argmin(lowerInliers[:, 0])
+    lowerXLimArgmax = np.argmax(lowerInliers[:, 0])
+    lowerXLims = (lowerInliers[lowerXLimArgmin, 0], lowerInliers[lowerXLimArgmax, 0])
 
-    for i in range(len(imgs)):
-        # convolve the image with the tread kernel and use RANSAC to find a cluster
-        #  of points that represents the threadform
-        # we do this twice because one of the kernels is upside down in order to
-        #  get the threads in the lower half of the image
-        lowerConv, _lowerInliers, lowerSlope, lowerIntercept = convoleAndRANSAC(imgs[i], lowerKernel)
-        upperConv, _upperInliers, upperSlope, upperIntercept = convoleAndRANSAC(imgs[i], upperKernel)
+    # start with a line between these two extreme points. take the average
+    #  intensity of the image across this line. step the line's y-intercept
+    #  closer to the centerline of the image, take the average intensity.
+    #  repeat until at the center.
+    lowerIntensityToCenter = getIntensityToCenter(img, lowerSlope, lowerIntercept, lowerXLims)
 
-        # combine the two convolution images, really just for visualizing
-        convs.append(lowerConv + upperConv)
+    # find the point along the average intensity curve from getIntensityToCenter
+    #  where we are out of the threads and onto the screw body
+    # TODO: this will need to change for silhouette images from the jig
+    lowerProbableEndOfThreadIndex = None
+    lowerIntensityToCenterDiff = np.diff(lowerIntensityToCenter)
+    j = 0
+    while (j < len(lowerIntensityToCenterDiff) and lowerProbableEndOfThreadIndex == None):
+        if (lowerIntensityToCenterDiff[j] >= 0):
+            lowerProbableEndOfThreadIndex = j
+        j += 1
 
-        # save the array of inlier points
-        lowerInliers.append(_lowerInliers)
-        upperInliers.append(_upperInliers)
+    if (lowerProbableEndOfThreadIndex == None):
+        raise RuntimeError("Couldn't find inside edge of threads!")
 
-        # calculate the points of a line for either side of the screw based on the
-        #  RANSAC results from above
-        x = np.arange(0, imgs[i].shape[1])
-        _lowerLineY = lowerSlope * x + lowerIntercept
-        _upperLineY = upperSlope * x + upperIntercept
-        lowerLineY.append(_lowerLineY)
-        upperLineY.append(_upperLineY)
+    # we want to use the intensity curve that's 20% of the way between the
+    #  initial line and the one identified above as the border between threads
+    #  and screw body
+    lowerThreadFrequencySampleIndex = int(0.2 * lowerProbableEndOfThreadIndex)
 
-        # find the x coordinates of the furthest left and right inlier point
-        lowerXLimArgmin = np.argmin(_lowerInliers[:, 0])
-        lowerXLimArgmax = np.argmax(_lowerInliers[:, 0])
-        lowerXLims = (_lowerInliers[lowerXLimArgmin, 0], _lowerInliers[lowerXLimArgmax, 0])
+    lowerThreadFrequencySample = getLineOfPixels(img, lowerSlope, lowerIntercept, -lowerThreadFrequencySampleIndex, lowerXLims)
 
-        # start with a line between these two extreme points. take the average
-        #  intensity of the image across this line. step the line's y-intercept
-        #  closer to the centerline of the image, take the average intensity.
-        #  repeat until at the center.
-        _lowerIntensityToCenter = getIntensityToCenter(imgs[i], lowerSlope, lowerIntercept, lowerXLims)
-        lowerIntensityToCenter.append(_lowerIntensityToCenter)
+    # take the fft of this intensity curve - the strongest response (other
+    #  than DC bias) should be the frequency of the threads
+    fft = np.abs(np.fft.fft(lowerThreadFrequencySample))
 
-        # find the point along the average intensity curve from getIntensityToCenter
-        #  where we are out of the threads and onto the screw body
-        # TODO: this will need to change for silhouette images from the jig
-        lowerProbableEndOfThreadIndex = None
-        lowerIntensityToCenterDiff = np.diff(_lowerIntensityToCenter)
-        j = 0
-        while (j < len(lowerIntensityToCenterDiff) and lowerProbableEndOfThreadIndex == None):
-            if (lowerIntensityToCenterDiff[j] >= 0):
-                lowerProbableEndOfThreadIndex = j
-            j += 1
+    # use the upper and lower y-intercepts from RANSAC to determine the
+    #  screw diameter
+    diameterPixels = abs(lowerIntercept - upperIntercept)
 
-        if (lowerProbableEndOfThreadIndex == None):
-            raise RuntimeError("Couldn't find inside edge of threads!")
+    # find the largest peak in the FFT (other than the DC bias in the center)
+    #  and find the period in pixels that corresponds to it, that's our
+    #  thread pitch
+    freqs = np.fft.fftfreq(len(fft))
+    maxMagnitudeIndex = np.argmax(fft[int(len(fft) / 2 + 1) : len(fft)]) + int(len(fft) / 2 + 1)
+    threadPitchFrequency = abs(freqs[maxMagnitudeIndex])
+    threadPitchPixels = 1 / threadPitchFrequency
 
-        # we want to use the intensity curve that's 20% of the way between the
-        #  initial line and the one identified above as the border between threads
-        #  and screw body
-        lowerThreadFrequencySampleIndex = int(0.2 * lowerProbableEndOfThreadIndex)
+    if (plotParams):
+        subplotRows = 4
+        subplotCols = plotParams['numImages']
+        imageIndex = plotParams['imageIndex']
 
-        lowerThreadFrequencySample = getLineOfPixels(imgs[i], lowerSlope, lowerIntercept, -lowerThreadFrequencySampleIndex, lowerXLims)
-        lowerThreadFrequencySamples.append(lowerThreadFrequencySample)
-
-        # take the fft of this intensity curve - the strongest response (other
-        #  than DC bias) should be the frequency of the threads
-        fft = np.abs(np.fft.fft(lowerThreadFrequencySample))
-        ffts.append(fft)
-
-        # use the upper and lower y-intercepts from RANSAC to determine the
-        #  screw diameter
-        diameter = abs(lowerIntercept - upperIntercept)
-        diameters.append(diameter)
-
-        print(str(i+1) + "/" + str(len(imgs)))
-
-    subplotRows = 4
-    subplotCols = len(imgs)
-
-    for i in range(len(imgs)):
-        plt.subplot(subplotRows, subplotCols, i + 1)
-        plt.imshow(imgs[i])
-        plt.plot(upperInliers[i][:, 0], upperInliers[i][:, 1], linewidth = 0, marker = '.', color = 'green')
-        plt.plot(lowerInliers[i][:, 0], lowerInliers[i][:, 1], linewidth = 0, marker = '.', color = 'red')
-        plt.plot(lowerLineY[i], color = 'yellow')
-        plt.plot(upperLineY[i], color = 'magenta')
+        plt.subplot(subplotRows, subplotCols, imageIndex + 1)
+        plt.imshow(img)
+        plt.plot(upperInliers[:, 0], upperInliers[:, 1], linewidth = 0, marker = '.', color = 'green')
+        plt.plot(lowerInliers[:, 0], lowerInliers[:, 1], linewidth = 0, marker = '.', color = 'red')
+        plt.plot(lowerLineY, color = 'yellow')
+        plt.plot(upperLineY, color = 'magenta')
         
-        plt.subplot(subplotRows, subplotCols, i + 1 + len(imgs))
-        plt.imshow(convs[i] + imgs[i])
-        plt.plot(upperInliers[i][:, 0], upperInliers[i][:, 1], linewidth = 0, marker = '.', color = 'green')
-        plt.plot(lowerInliers[i][:, 0], lowerInliers[i][:, 1], linewidth = 0, marker = '.', color = 'red')
-        plt.plot(lowerLineY[i], color = 'yellow')
-        plt.plot(upperLineY[i], color = 'magenta')
+        plt.subplot(subplotRows, subplotCols, imageIndex + 1 + subplotCols)
+        plt.imshow(conv + img)
+        plt.plot(upperInliers[:, 0], upperInliers[:, 1], linewidth = 0, marker = '.', color = 'green')
+        plt.plot(lowerInliers[:, 0], lowerInliers[:, 1], linewidth = 0, marker = '.', color = 'red')
+        plt.plot(lowerLineY, color = 'yellow')
+        plt.plot(upperLineY, color = 'magenta')
 
-        plt.subplot(subplotRows, subplotCols, i + 1 + len(imgs) * 2)
-        plt.plot(lowerThreadFrequencySamples[i])
+        plt.subplot(subplotRows, subplotCols, imageIndex + 1 + subplotCols * 2)
+        plt.plot(lowerThreadFrequencySample)
 
-        plt.subplot(subplotRows, subplotCols, i + 1 + len(imgs) * 3)
-        freqs = np.fft.fftfreq(len(ffts[i]))
-        plt.plot(freqs, ffts[i])
+        plt.subplot(subplotRows, subplotCols, imageIndex + 1 + subplotCols * 3)
+        plt.plot(freqs, fft)
 
-        # find the largest peak in the FFT (other than the DC bias in the center)
-        #  and find the period in pixels that corresponds to it, that's our
-        #  thread pitch
-        maxMagnitudeIndex = np.argmax(ffts[i][int(len(ffts[i]) / 2 + 1) : len(ffts[i])]) + int(len(ffts[i]) / 2 + 1)
-        threadPitchFrequency = abs(freqs[maxMagnitudeIndex])
-        threadPitchPixels = 1 / threadPitchFrequency
-        print(str(i) + " pitch: " + str(threadPitchPixels) + " pixels")
+    return diameterPixels, threadPitchPixels
 
-        print(str(i) + " diameter: " + str(diameters[i]) + " pixels")
-        print("")
+def main():
+    if (len(sys.argv) < 3):
+        printUsage()
+
+    kernelImgFile = sys.argv[1]
+    kernelImg = ImageOps.grayscale(Image.open(kernelImgFile))
+
+    numImgs = len(sys.argv) - 2
+    imgs = []
+    plotParams = {'numImages': numImgs}
+
+    for i in range(numImgs):
+        file = sys.argv[i+2]
+        imgs.append(Image.open(file))
+
+    for i in range(len(imgs)):
+        plotParams['imageIndex'] = i
+        diameterPixels, threadPitchPixels = identify(imgs[i], kernelImg, plotParams)
+        print("===================")
+        print("Image " + str(i + 1))
+        print("Pitch (pixels): " + str(threadPitchPixels))
+        print("Diameter (pixels): " + str(diameterPixels))
+
+    print("===================")
 
     plt.show()
-
 
 if __name__ == "__main__":
     main()
